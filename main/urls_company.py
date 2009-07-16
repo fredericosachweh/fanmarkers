96b4ed829fcfe5711550114d351027d9a9e16241
chris <chris@b8fb8579-4c80-4443-9e11-82fcd885edea>
2009-07-16 05:16:04 +0000

    new/edit company generic view wrapper
    git-svn-id: http://svn.fanmarkers.com@117 b8fb8579-4c80-4443-9e11-82fcd885edea

--- main/urls_company.py
+++ main/urls_company.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('main',
-	url(r'^$',					"views_company.make_list", name="list-company"),
-	url(r'^(?P<object_id>\d{1,4})/$',		"views_company.view", name="view-company"),
-	url(r'^new/$',					"views_company.new", name="new-company"),
-	url(r'^edit/(?P<object_id>\d{1,4})/$',		"views_company.edit", name="edit-company"),
+	url(r'^$',				"views_company.make_list", name="list-company"),
+	url(r'^(?P<pk>\d{1,4})/$',		"views_company.view", name="view-company"),
+	url(r'^new/$',				"views_company.new", name="new-company"),
+	url(r'^edit/(?P<pk>\d{1,4})/$',		"views_company.edit", name="edit-company"),
 )
--- main/views_company.py
+++ main/views_company.py
@@ -1,12 +1,13 @@
 from annoying.decorators import render_to
+from django.shortcuts import get_object_or_404
+from django.views.generic.create_update import update_object, create_object
+
 from models import Company
-from forms import CompanySearch
+from forms import CompanySearch, CompanyForm
 
 @render_to('list_aircraft-company.html')
 def make_list(request):
-
 	type="Company"
-	
 	objects = Company.objects.all()
 	
 	if request.GET:
@@ -23,18 +24,22 @@ def make_list(request):
 		 
 		 	if searchform.cleaned_data["search"]:
 				s = searchform.cleaned_data["search"]
-				objects = objects.filter( Q(name__icontains=s) | Q(description__icontains=s) )
-				
+				objects = objects.filter( Q(name__icontains=s) | Q(description__icontains=s) )	
 	else:
 		searchform = CompanySearch()
 
 	return locals()
 	
-def edit(request):
-	pass
+########################################################
 	
+def edit(request, pk):
+	return update_object(request, object_id=pk, form_class=CompanyForm, template_name='edit_company.html')
+
 def new(request):
-	pass
+	return create_object(request, form_class=CompanyForm, template_name='new_company.html')
 	
-def view(request):
-	pass
+@render_to('view_company.html')	
+def view(request, pk):
+	company = get_object_or_404(Company, pk=pk)
+	
+	return locals()
