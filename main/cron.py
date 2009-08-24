a51d6b05c0a8f96ceb9dd10e6806daf468f1f568
Chris Priest <cp368202@ohiou.edu>
2009-08-23 20:22:15 -0400

    bug fixes in cron

--- main/cron.py
+++ main/cron.py
@@ -3,6 +3,7 @@ from dateutil.relativedelta import relativedelta
 from datetime import date
 from models import Status
 from django.db.models import Q
+from settings import PROJECT_DIR
 
 class UpdateHiring(CronJob):
     "Cron Job that goes through every twenve hours and changes all statuses that are older than 30 days"
@@ -13,13 +14,11 @@ class UpdateHiring(CronJob):
     def job(self):
         cutoff_date = date.today() + relativedelta(days=-10)                    #the date of 30 days ago
         
-        hiring_old = Status.objects.filter(
-            Q(last_modified__lte=cutoff_date) & (Q(assign_bases__isnull=False) | Q(choice_bases__isnull=False))
-        )
+        hiring_old = Status.objects.filter(Q(last_modified__lte=cutoff_date) & (Q(assign_bases__isnull=False) | Q(choice_bases__isnull=False)))
         
         if hiring_old:
-            logfile = open(PROJECT_DIR + 'status_cron.log', 'a')
-            logfile.write(date.today() + " -------------\n")
+            logfile = open(PROJECT_PATH + 'status_cron.log', 'a')
+            logfile.write(str(date.today()) + " -------------\n")
         
             for status in hirig_old:
                 logfile.write(status + "\n")
@@ -28,7 +27,7 @@ class UpdateHiring(CronJob):
             
             logfile.close()
         else:
-            logfile = open(PROJECT_DIR + 'status_cron.log', 'a')
-            logfile.write(date.today() + " nothing\n")
+            logfile = open(PROJECT_PATH + 'status_cron.log', 'a')
+            logfile.write(str(date.today()) + " nothing\n")
 
 cronScheduler.register(UpdateHiring)
