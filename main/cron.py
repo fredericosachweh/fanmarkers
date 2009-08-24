cfae16d3e625598adfcc4e72e0a8c6532d0d2026
Chris Priest <cp368202@ohiou.edu>
2009-08-23 20:08:21 -0400

    modified cron app to work with psygopg2

--- main/cron.py
+++ main/cron.py
@@ -1,10 +1,10 @@
-from django_cron import cronScheduler, Job
+from django_cron import cronScheduler, CronJob
 from dateutil.relativedelta import relativedelta
 from datetime import date
 from models import Status
 from django.db.models import Q
 
-class UpdateHiring(Job):
+class UpdateHiring(CronJob):
     "Cron Job that goes through every twenve hours and changes all statuses that are older than 30 days"
 
     # run every (12 hours)
@@ -13,9 +13,9 @@ class UpdateHiring(Job):
     def job(self):
         cutoff_date = date.today() + relativedelta(days=-10)                    #the date of 30 days ago
         
-        hiring_old = False #Status.objects.filter(
-        #    Q(last_modified__lte=cutoff_date) & (Q(assign_bases__isnull=False) | Q(choice_bases__isnull=False))
-        #)
+        hiring_old = Status.objects.filter(
+            Q(last_modified__lte=cutoff_date) & (Q(assign_bases__isnull=False) | Q(choice_bases__isnull=False))
+        )
         
         if hiring_old:
             logfile = open(PROJECT_DIR + 'status_cron.log', 'a')
