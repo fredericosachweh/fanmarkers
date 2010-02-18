2fd5bb81ed9ca144fd803674d219bae306f7553a
chris <chris@chris-comp.(none)>
2010-02-18 01:14:16 -0500

    more javascript code cleanup

--- media/javascripts/big_map.js
+++ media/javascripts/big_map.js
@@ -81,49 +81,63 @@ var labels =
 	]
 	
 
+function make_json(data, responseCode) {
+	
+	json = eval(data);
+	
+	var mytabs = [
+		new MaxContentTab('Based Here ' + json["base_t"], json["base"]),               
+		new MaxContentTab('Flies Here ' + json["route_t"], json["route"])
+		];
+
+	map.openMaxContentTabs(
+	    point,
+        json["title"],
+        json["title"],
+        mytabs,
+        {width: "300px"}
+    );
+}
+
 function initialize()
 {
 	if (GBrowserIsCompatible())
 	{
-		map = new GMap2(document.getElementById("map_canvas"));
-		var polyOptions = {geodesic:true};
+	map = new GMap2(document.getElementById("map_canvas"));
+	var polyOptions = {geodesic:true};
+	
+    //move the controls down so they are below the menu bar
+	var topLeft = new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10,30));
+	var topRight = new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10,30));
 		
-		var topLeft = new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10,30));		//move the controls down so they are below the menu bar
-		var topRight = new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10,30));
-			
-		map.setCenter(new GLatLng(10,0), 2);
-		
-		map.addMapType(G_PHYSICAL_MAP);
-		map.setMapType(G_PHYSICAL_MAP);
-		
-		map.addControl(new GScaleControl());
-		map.addControl(new GLargeMapControl3D(), topLeft);
-		map.addControl(new GMapTypeControl(), topRight);
-		map.enableContinuousZoom();
-		map.enableScrollWheelZoom();
+	map.setCenter(new GLatLng(10,0), 2);
+	
+	map.addMapType(G_PHYSICAL_MAP);
+	map.setMapType(G_PHYSICAL_MAP);
+	
+	map.addControl(new GScaleControl());
+	map.addControl(new GLargeMapControl3D(), topLeft);
+	map.addControl(new GMapTypeControl(), topRight);
+	map.enableContinuousZoom();
+	map.enableScrollWheelZoom();
 
-		G_PHYSICAL_MAP.getMinimumResolution = function () { return 2 };
-		G_NORMAL_MAP.getMinimumResolution = function () { return 2 };
-		G_SATELLITE_MAP.getMinimumResolution = function () { return 2 };
-		G_HYBRID_MAP.getMinimumResolution = function () { return 2 };
+	G_PHYSICAL_MAP.getMinimumResolution = function () { return 2 };
+	G_NORMAL_MAP.getMinimumResolution = function () { return 2 };
+	G_SATELLITE_MAP.getMinimumResolution = function () { return 2 };
+	G_HYBRID_MAP.getMinimumResolution = function () { return 2 };
 
-		figure_zoom();
+	figure_zoom();
+	
+	GEvent.addListener(map, "zoomend", figure_zoom);
+	
+	GEvent.addListener(map, "click", function(marker, point) {
+	    
+	    url = sprintf("/map_click/%s_%s_%s/",
+                      map.getZoom(), point.lat(), point.lng())
+    
+        GDownloadUrl(url, make_json)
 		
-		GEvent.addListener(map, "zoomend", figure_zoom);
-		
-		GEvent.addListener(map, "click", function(marker, point) {
-			GDownloadUrl("/map_click/" + map.getZoom() + "_" + point.lat() + "_" + point.lng() + "/",
-			
-			function(data, responseCode) {
-				json = eval(data);
-				var mytabs = [
-						new MaxContentTab('Based Here ' + json["base_t"],	json["base"]),
-						new MaxContentTab('Flies Here ' + json["route_t"],	json["route"])
-					];
-
-				map.openMaxContentTabs(point, json["title"], json["title"], mytabs, {width: "300px"});
-			});
-		});
+	});
 	}
 }
 
